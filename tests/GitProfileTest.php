cc102085e7e67654d0e9b5d5eba6b3b7c8b49a3d
Zeeshan Ahmed <ziishaned@gmail.com>
2016-09-17 07:00:03 +0500

    Add UnitTest for GitProfile

---
+++ tests/GitProfileTest.php
@@ -0,0 +1,32 @@
+<?php 
+
+namespace Tests;
+
+use Zeeshan\GitProfile\GitProfile;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Console\Application;
+
+class GitProfileTest extends \PHPUnit_Framework_TestCase
+{
+	private $app;
+
+	protected function setUp()
+    {
+        $this->app = new Application('test', false);
+        $this->app->setAutoExit(false);
+    }
+
+	public function testGetCommandsShouldReturnArray()
+	{
+		$gitprofile = new GitProfile();
+		$this->assertNotEmpty($gitprofile->getCommands());
+	}
+
+	public function testCanRunProfileBaseCommand()
+	{
+		$process = new Process('php git-profile');
+		$process->run();
+
+		$this->assertEquals(0, $process->getExitCode());
+	}
+}
