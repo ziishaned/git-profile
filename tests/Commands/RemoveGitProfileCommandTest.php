bfdd8ec1400ed4997a93f2eeef189759638c9309
Zeeshan Ahmed <ziishaned@gmail.com>
2016-09-17 09:06:16 +0500

    Add unittests

--- tests/Commands/RemoveGitProfileCommandTest.php
+++ tests/Commands/RemoveGitProfileCommandTest.php
@@ -5,6 +5,7 @@ namespace Tests\Commands;
 use Zeeshan\GitProfile\GitProfile;
 use Symfony\Component\Process\Process;
 use Symfony\Component\Console\Application;
+use Zeeshan\GitProfile\Commands\AddGitProfileCommand;
 use Zeeshan\GitProfile\Commands\RemoveGitProfileCommand;
 
 class RemoveGitProfileCommandTest extends \PHPUnit_Framework_TestCase
@@ -14,11 +15,25 @@ class RemoveGitProfileCommandTest extends \PHPUnit_Framework_TestCase
 	protected function setUp()
     {
         $this->command = new RemoveGitProfileCommand();
+    	(new AddGitProfileCommand)->saveProfile('Personal', 'Zeeshan Ahmed', 'ziishaned@gmail.com');
     }
 
     public function testRemoveProfileShouldRemoveGitProfile()
     {
     	$result = $this->command->removeProfile('Personal');
+    	$this->assertTrue($result);
+    }
+
+    public function testDoesProfileExistsMustReturnFalseIfProfileNotExists()
+    {
+    	$result = $this->command->doesProfileExists('xyz');
+    	$this->assertFalse($result);
+    }
+
+    public function testDoesProfileExistsMustReturnTrueIfProfileExists()
+    {
+    	$this->setUp();
+    	$result = $this->command->doesProfileExists('Personal');
     	$this->assertTrue($result);
     }
 }
