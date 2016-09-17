33df728dbd22f17d4db5051d5ecd75a0fa93b5bd
Zeeshan Ahmed <ziishaned@gmail.com>
2016-09-17 14:27:36 +0500

    Register new commands

--- src/GitProfile.php
+++ src/GitProfile.php
@@ -6,7 +6,9 @@ use Symfony\Component\Console\Application;
 use Zeeshan\GitProfile\Commands\GitProfileCommand;
 use Zeeshan\GitProfile\Commands\AddGitProfileCommand;
 use Zeeshan\GitProfile\Commands\UseGitProfileCommand;
+use Zeeshan\GitProfile\Commands\ShowGitProfileCommand;
 use Zeeshan\GitProfile\Commands\RemoveGitProfileCommand;
+use Zeeshan\GitProfile\Commands\UpdateGitProfileCommand;
 // use Zeeshan\GitProfile\Commands\ListGitProfilesCommand;
 
 /**
@@ -37,6 +39,8 @@ class GitProfile
 		$this->commands[] = new AddGitProfileCommand();
 		$this->commands[] = new UseGitProfileCommand();
 		$this->commands[] = new RemoveGitProfileCommand();
+		$this->commands[] = new ShowGitProfileCommand();
+		$this->commands[] = new UpdateGitProfileCommand();
 		// $this->commands[] = new ListGitProfilesCommand();
 	}
 
