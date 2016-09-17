1eebc205d16971f2e8fcdc45e3bea931910a2b46
Zeeshan Ahmed <ziishaned@gmail.com>
2016-09-17 15:37:19 +0500

    Add CurrentGitProfileCommand

---
+++ src/Commands/CurrentGitProfileCommand.php
@@ -0,0 +1,79 @@
+<?php 
+
+namespace Zeeshan\GitProfile\Commands;
+
+use Zeeshan\GitProfile\Commands\BaseCommand;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * @package GitProfile
+ * @author  Zeeshan Ahmed<ziishaned@gmail.com>
+ */
+class CurrentGitProfileCommand extends BaseCommand
+{
+
+	/**
+	 * Configure the command
+	 * 
+	 * @return void
+	 */
+	public function configure()
+	{
+		$this->setName('current')
+			 ->setDescription('Get the current profile.')
+			 ->addOption('global', null, InputOption::VALUE_NONE, 'Set git profile global.');
+	}
+
+	/**
+	 * Execute the command
+	 * 
+	 * @param  Symfony\Component\Console\Input\InputInterface  $input  
+	 * @param  Symfony\Component\Console\Output\OutputInterface $output 
+	 * @return void                  
+	 */
+	public function execute(InputInterface $input, OutputInterface $output)
+	{
+
+		$style = new SymfonyStyle($input, $output);
+		
+		if ($input->getOption('global')) {
+			$currentProfile = $this->reteriveCurrentProfile('global');
+		}
+
+		$currentProfile = $this->reteriveCurrentProfile();
+
+		if ($this->doesProfileExists($currentProfile)) {
+			$email =  $this->runCommand(sprintf('git config --global profile.%s.email', $currentProfile));
+			$name  =  $this->runCommand(sprintf('git config --global profile.%s.name', $currentProfile));
+
+			$output->writeln('');
+			$output->writeln('[+] Current Profile: ' . $currentProfile);
+			$output->writeln('[+] Name: ' . $name);
+			$output->writeln('[+] Email: ' . $email);
+			exit(1);
+		}
+
+		$style->error('Something went wrong.');
+	}
+
+	/**
+	 * Check wether or not git profile exist
+	 * 
+	 * @param  string $profileTitle
+	 * @return boolean
+	 */
+	public function doesProfileExists($profileTitle)
+	{
+		$commandOutput = $this->runCommand('git config -l --name-only');
+
+		if (stripos($commandOutput, "profile." . $profileTitle)) {
+			return true;
+		}		
+		
+		return false;
+	}
+}
